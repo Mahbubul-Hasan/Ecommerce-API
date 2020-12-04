8dab0eb86c9f79ecaeb3c180fc4618f108d0dfb5
Mahbubul Hasan <mahbubul161@gmail.com>
2020-12-04 11:56:28 +0600

    Change: API resource for products table

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,11 +15,16 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name' => $this->name,
-            'details' => $this->details,
-            'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount,
+            'name'       => $this->name,
+            'details'    => $this->details,
+            'price'      => $this->price,
+            'stock'      => $this->stock == 0 ? "Out of Stock!" : $this->stock,
+            'discount'   => $this->discount,
+            'totalPrice' => round($this->price * (1 - ($this->discount / 100)), 2),
+            'rating'     => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count()) : 'No rating yet',
+            'href'       => [
+                'reviews' => route('reviews.index', $this->id),
+            ],
         ];
     }
 }
