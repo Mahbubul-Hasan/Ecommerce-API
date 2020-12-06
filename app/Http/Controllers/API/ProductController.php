634b92114dba0eeda6abe23afc098d409b3fb74c
Mahbubul Hasan <mahbubul161@gmail.com>
2020-12-06 23:05:24 +0600

    Add: auth:api middleware in productcontroller

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -10,6 +10,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+    }
     /**
      * Display a listing of the resource.
      *
